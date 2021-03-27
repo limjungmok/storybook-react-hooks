66d4f82aea9fda980d2e4249288c93545f2c118f
limjungmok <limjungmok@nts-corp.com>
2021-03-27 12:27:37 +0900

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useResize } from './hooks/useResize';
+
 import './App.css';
 
 function App() {
+  const { width, height } = useResize();
+
+  console.log('useResize', width, height);
   return (
     <div className="App">
       <header className="App-header">
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ResizeProvider from './hooks/useResize';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ResizeProvider>
     <App />
-  </React.StrictMode>,
+  </ResizeProvider>,
   document.getElementById('root')
 );
 
